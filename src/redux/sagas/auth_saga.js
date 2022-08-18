790af10073b35abefad52ff23359d2522449b0c2
hungtrn75 <hungtrn75@gmail.com>
2022-08-18 08:58:50 +0700

    fix: check token is nullable

--- src/redux/sagas/auth_saga.js
+++ src/redux/sagas/auth_saga.js
@@ -1,98 +1,103 @@
-import NetInfo from "@react-native-community/netinfo";
-import axios from "axios";
-import { showMessage } from "react-native-flash-message";
-import { all, call, put, select, takeLatest } from "redux-saga/effects";
-import { BASE_URL, resetEndpoint } from "../../constants";
-import { insertQuery, selectQuery } from "../../database/sqlite";
-import { goBack, replace } from "../../navigator/helper";
-import { AuthService, LayerService } from "../../service";
-import CBNative from "../../utils/CBNative";
-import { networkHandler } from "../../utils/ErrorHandler";
-import { pushMessage } from "../../utils/flash_message";
-import AppStorage from "../../utils/storage";
-import { settingTypes } from "../state/setting_redux";
-import { authTypes } from "./../state/auth_redux";
+import NetInfo from '@react-native-community/netinfo';
+import axios from 'axios';
+import {showMessage} from 'react-native-flash-message';
+import {all, call, put, select, takeLatest} from 'redux-saga/effects';
+import {BASE_URL, resetEndpoint} from '../../constants';
+import {insertQuery, selectQuery} from '../../database/sqlite';
+import {goBack, replace} from '../../navigator/helper';
+import {AuthService, LayerService} from '../../service';
+import CBNative from '../../utils/CBNative';
+import {networkHandler} from '../../utils/ErrorHandler';
+import {pushMessage} from '../../utils/flash_message';
+import AppStorage from '../../utils/storage';
+import {settingTypes} from '../state/setting_redux';
+import {authTypes} from './../state/auth_redux';
 
 function* loginWorker(action) {
   try {
     CBNative.show();
-    const { isInternetReachable } = yield call(NetInfo.fetch);
+    const {isInternetReachable} = yield call(NetInfo.fetch);
     if (isInternetReachable) {
       const userData = action.payload;
       const resLogin = yield call(AuthService.login, userData);
-      yield call(AppStorage.setToken, resLogin.access_token);
-      const [resMe, pendingFeatures, { layers, features }] = yield all(
-        [
+      if (resLogin.access_token) {
+        yield call(AppStorage.setToken, resLogin.access_token);
+        const [resMe, pendingFeatures, {layers, features}] = yield all([
           call(AuthService.me),
           call(LayerService.getPendingFeatures),
           call(LayerService.getDoneFeatures),
-        ],
-      );
-      const [banDoNen, bookmarks] = yield all(
-        [
+        ]);
+        const [banDoNen, bookmarks] = yield all([
           call(LayerService.layBanDoNen),
           call(LayerService.getBookmarks),
-        ],
-      );
-      yield call(AppStorage.setVal, "resMe", JSON.stringify(resMe));
-      yield put({
-        type: authTypes.REQUEST_LOGIN_SUCCESS,
-        payload: resMe,
-      });
-      yield put({
-        type: settingTypes.BAN_DO_NEN,
-        payload: banDoNen,
-      });
-      const tPendingFeatures = pendingFeatures.map(el => ({
-        feature_id: el.id,
-        json: JSON.stringify(el),
-      }));
-      const tFeatures = features.map(el => ({
-        feature_id: el.id,
-        json: JSON.stringify(el),
-      }));
-      const tLayers = layers.map(el => ({
-        layer_id: el.id,
-        json: JSON.stringify(el),
-      }));
-      yield call(insertQuery, [], tPendingFeatures, "tbl_pending_features");
-      yield call(insertQuery, [], tFeatures, "tbl_features");
-      yield call(insertQuery, [], tLayers, "tbl_layers");
+        ]);
+        yield call(AppStorage.setVal, 'resMe', JSON.stringify(resMe));
+        yield put({
+          type: authTypes.REQUEST_LOGIN_SUCCESS,
+          payload: resMe,
+        });
+        yield put({
+          type: settingTypes.BAN_DO_NEN,
+          payload: banDoNen,
+        });
+        const tPendingFeatures = pendingFeatures.map(el => ({
+          feature_id: el.id,
+          json: JSON.stringify(el),
+        }));
+        const tFeatures = features.map(el => ({
+          feature_id: el.id,
+          json: JSON.stringify(el),
+        }));
+        const tLayers = layers.map(el => ({
+          layer_id: el.id,
+          json: JSON.stringify(el),
+        }));
+        yield call(insertQuery, [], tPendingFeatures, 'tbl_pending_features');
+        yield call(insertQuery, [], tFeatures, 'tbl_features');
+        yield call(insertQuery, [], tLayers, 'tbl_layers');
 
-      const { rows } = yield call(selectQuery, "SELECT * FROM tbl_drafts", []);
+        const {rows} = yield call(selectQuery, 'SELECT * FROM tbl_drafts', []);
 
-      let drafts = [];
-      for (let i = 0; i < rows.length; i++) {
-        var item = rows.item(i);
-        drafts.push({
-          ...item,
-          json: JSON.parse(item.json),
+        let drafts = [];
+        for (let i = 0; i < rows.length; i++) {
+          var item = rows.item(i);
+          drafts.push({
+            ...item,
+            json: JSON.parse(item.json),
+          });
+        }
+
+        yield put({
+          type: settingTypes.GET_FEATURES_SUCCESS,
+          payload: {layers, features},
+        });
+        yield put({
+          type: settingTypes.FETCH_DRAFTS_SUCCESS,
+          payload: drafts,
+        });
+        yield put({
+          type: settingTypes.FETCH_PENDING_FEARTURES_SUCCESS,
+          payload: pendingFeatures,
+        });
+
+        yield put({
+          type: settingTypes.GET_BOOKMARKS_SUCCESS,
+          payload: bookmarks,
+        });
+        replace('TAB');
+      } else {
+        showMessage({
+          message: 'Đăng nhập thất bại',
+          description: 'Kiểm tra lại kết nối của bạn',
+          type: 'danger',
+          icon: {icon: 'danger', position: 'left'},
         });
       }
-
-      yield put({
-        type: settingTypes.GET_FEATURES_SUCCESS,
-        payload: { layers, features },
-      });
-      yield put({
-        type: settingTypes.FETCH_DRAFTS_SUCCESS,
-        payload: drafts,
-      });
-      yield put({
-        type: settingTypes.FETCH_PENDING_FEARTURES_SUCCESS,
-        payload: pendingFeatures,
-      });
-
-      yield put({
-        type: settingTypes.GET_BOOKMARKS_SUCCESS,
-        payload: bookmarks,
-      });
-      replace("TAB");
     } else {
-      replace("AUTH_CONTAINER");
+      replace('AUTH_CONTAINER');
     }
   } catch (error) {
-    networkHandler(error, "Đăng nhập thất bại vui lòng thử lại sau");
+    networkHandler(error, 'Đăng nhập thất bại vui lòng thử lại sau');
   } finally {
     CBNative.hide();
   }
@@ -105,9 +110,9 @@ export function* loginWatcher() {
 function* logoutWorker(action) {
   try {
     yield call(AppStorage.removeToken);
-    replace("AUTH_CONTAINER");
+    replace('AUTH_CONTAINER');
   } catch (error) {
-    console.log("auth", error);
+    console.log('auth', error);
   }
 }
 
@@ -125,7 +130,7 @@ function* updateProfileWorker(action) {
       payload: resMe,
     });
   } catch (error) {
-    console.log("auth", error);
+    console.log('auth', error);
   } finally {
     CBNative.hide();
   }
@@ -145,7 +150,7 @@ function* changeAvatarWorker(action) {
       payload: resMe,
     });
   } catch (error) {
-    console.log("auth", error, { ...error });
+    console.log('auth', error, {...error});
   } finally {
     CBNative.hide();
   }
@@ -158,33 +163,33 @@ export function* changeAvatarWatcher() {
 //@ts-ignore
 function* getMeWorker() {
   try {
-    const baseUrl = yield call(AppStorage.getVal, "base_url");
+    const baseUrl = yield call(AppStorage.getVal, 'base_url');
     if (baseUrl) {
       yield call(resetEndpoint, baseUrl);
     } else {
       axios.defaults.baseURL = BASE_URL;
     }
     const token = yield call(AppStorage.getToken);
-    const { isConnected } = yield call(NetInfo.fetch);
+    const {isConnected} = yield call(NetInfo.fetch);
 
     // ** \\
     let pendingFeatures = [],
       features = [],
       layers = [];
 
-    const { rows: rPendingFeatures } = yield call(
+    const {rows: rPendingFeatures} = yield call(
       selectQuery,
-      "SELECT * FROM tbl_pending_features",
+      'SELECT * FROM tbl_pending_features',
       [],
     );
-    const { rows: rFeatures } = yield call(
+    const {rows: rFeatures} = yield call(
       selectQuery,
-      "SELECT * FROM tbl_features",
+      'SELECT * FROM tbl_features',
       [],
     );
-    const { rows: rLayers } = yield call(
+    const {rows: rLayers} = yield call(
       selectQuery,
-      "SELECT * FROM tbl_layers",
+      'SELECT * FROM tbl_layers',
       [],
     );
 
@@ -204,17 +209,17 @@ function* getMeWorker() {
         layers.push(JSON.parse(item.json));
       }
     } else {
-      if (isConnected){
-        [pendingFeatures, { layers, features }] = yield all([
+      if (isConnected) {
+        [pendingFeatures, {layers, features}] = yield all([
           call(LayerService.getPendingFeatures),
           call(LayerService.getDoneFeatures),
         ]);
       }
     }
-    let banDoNen = yield call(LayerService.layBanDoNen)
-    const { rows: rDrafts } = yield call(
+    let banDoNen = yield call(LayerService.layBanDoNen);
+    const {rows: rDrafts} = yield call(
       selectQuery,
-      "SELECT * FROM tbl_drafts",
+      'SELECT * FROM tbl_drafts',
       [],
     );
 
@@ -229,7 +234,7 @@ function* getMeWorker() {
 
     yield put({
       type: settingTypes.GET_FEATURES_SUCCESS,
-      payload: { layers, features },
+      payload: {layers, features},
     });
     yield put({
       type: settingTypes.FETCH_DRAFTS_SUCCESS,
@@ -246,7 +251,7 @@ function* getMeWorker() {
 
     if (isConnected) {
       if (!token) {
-        replace("AUTH_CONTAINER");
+        replace('AUTH_CONTAINER');
       } else {
         const [resMe, bookmarks] = yield all([
           call(AuthService.me),
@@ -260,24 +265,24 @@ function* getMeWorker() {
           type: settingTypes.GET_BOOKMARKS_SUCCESS,
           payload: bookmarks,
         });
-        replace("TAB");
+        replace('TAB');
       }
     } else {
-      const resMeStr = yield call(AppStorage.getVal, "resMe");
+      const resMeStr = yield call(AppStorage.getVal, 'resMe');
       if (resMeStr) {
         yield put({
           type: authTypes.REQUEST_LOGIN_SUCCESS,
           payload: JSON.parse(resMeStr),
         });
-        replace("TAB");
+        replace('TAB');
       } else {
-        replace("AUTH_CONTAINER");
+        replace('AUTH_CONTAINER');
       }
-      replace("AUTH_CONTAINER");
+      replace('AUTH_CONTAINER');
     }
   } catch (error) {
-    console.log("auth", error, { ...error });
-    replace("AUTH_CONTAINER");
+    console.log('auth', error, {...error});
+    replace('AUTH_CONTAINER');
   }
 }
 
@@ -292,21 +297,21 @@ function* changePasswordWorker(action) {
     yield call(AuthService.changePassword, userId, action.payload);
 
     pushMessage({
-      message: "Thay đổi mật khẩu thành công",
-      type: "success",
+      message: 'Thay đổi mật khẩu thành công',
+      type: 'success',
     });
     goBack();
   } catch (error) {
-    console.log("auth", error);
+    console.log('auth', error);
     const message =
-      error.response?.data.message ?? "Đăng nhập thất bại vui lòng thử lại sau";
+      error.response?.data.message ?? 'Đăng nhập thất bại vui lòng thử lại sau';
     if (message)
       showMessage({
         message,
-        type: "danger",
+        type: 'danger',
         icon: {
-          position: "center",
-          icon: "danger",
+          position: 'center',
+          icon: 'danger',
         },
       });
   } finally {
